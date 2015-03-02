13701f6980a8eaf02012a1a6600fd483aaa43e6f
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2015-03-02 16:36:17 +0100

    Try with real SemanticReleaseNotesParser

---
+++ SemanticReleaseNotesParser/Abstractions/WebClientWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+
+namespace SemanticReleaseNotesParser.Abstractions
+{
+    [ExcludeFromCodeCoverage]
+    internal sealed class WebClientWrapper : WebClient, IWebClient
+    {
+        public WebClientWrapper(string baseAddress)
+        {
+            BaseAddress = baseAddress;
+            Headers["Accept"] = "application/json";
+            Headers["Content-type"] = "application/json";
+        }
+    }
+}
---
+++ SemanticReleaseNotesParser/OutputType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SemanticReleaseNotesParser
+{
+    [Flags]
+    internal enum OutputType
+    {
+        File = 1,
+        Environment = 2,
+        FileAndEnvironment = File | Environment
+    }
+}
